be12d91674649c7f2574fe5ded3c49121ec01a2f
Crapteep <crapteep@gmail.com>
2023-06-30 02:03:00 +0200

    implemented a new way to fetch data using useQuery

--- frontend/src/pages/PostInfo/PostInfo.js
+++ frontend/src/pages/PostInfo/PostInfo.js
@@ -1,61 +1,47 @@
-
-import axios from "axios";
 import React from "react";
 import './PostInfo.css';
 
-import {useEffect, useState} from 'react'
-import { useParams } from "react-router-dom";
+import { useState } from 'react'
 import Loading from "../../components/Loading";
 import UserImages from "../../components/UserImages";
+import usePostData from "../../hooks/usePostData";
 
 
 const PostInfo = () => {
-    const { id } = useParams();
-    const [post, setPost] = useState([{}])
-    const [isLoading, setIsLoading] = useState(true);
     const [isHovered, setIsHovered] = useState(false);
 
-
-    useEffect(() => {
-        axios.get(`http://localhost:8000/posts/${id}`).then(res =>
-        setPost(res.data))
-        setIsLoading(false);
-      }, [id]);
+    const { isLoading, data } = usePostData();
       
-    
-      if (isLoading) {
-        return <Loading />;
-        
-    }
 
     let hoverTimeout;
     const handleCardMouseEnter = () => {
         setIsHovered(true);
         clearTimeout(hoverTimeout);
-
     };
 
     const handleCardMouseLeave = () => {
         hoverTimeout = setTimeout(() => setIsHovered(false), 600);
     };
 
+    if (isLoading) {
+        return <Loading />;
+    }
+
     return (
         <div className="container">
-        {post?.users?.map((user) => (
-        <div className="row " key={user.name}>
-            <div className={`card my-3 border-2 border-dark fw-bold ${isHovered ? "hover-zoom" : ""}`}
+        {data?.users?.map((user) => (
+        <div className={`row card my-3 border-2 border-dark fw-bold ${isHovered ? "hover-zoom" : ""}`} 
+            key={user.name}
             onMouseEnter={handleCardMouseEnter}
-            onMouseLeave={handleCardMouseLeave}
-            >
-
+            onMouseLeave={handleCardMouseLeave}>
+            
             <h5 className="card-header ">{user.name}</h5>
             <div className="card-body ">
-          
+
                 <UserImages 
-                    username={user.name}
+                    user={user}
                 />
 
-            </div>
             </div>
         </div>
         ))}

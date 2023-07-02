f4b4196cdef03984c1e8f59f48b5eb884e1561cb
Crapteep <crapteep@gmail.com>
2023-07-02 16:34:52 +0200

    Several deployment bugs have been fixed 2

--- frontend/src/components/DisplaySingleUserImage.css/DisplaySingleUserImage.js
+++ frontend/src/components/DisplaySingleUserImage.css/DisplaySingleUserImage.js
@@ -5,7 +5,7 @@ const DisplaySingleUserImage = ( {image} ) => {
         <div className="col-12 col-sm-10 col-md-10 col-lg-4 mb-2 mt-2 d-flex justify-content-center align-items-center">
             <img 
                 src={`http://localhost:8000/static/${image}`}
-                alt={`Box Image`}
+                alt={`Box`}
                 className="img-fluid fixed-size-image border border-gray-400"/>
         </div>
     )

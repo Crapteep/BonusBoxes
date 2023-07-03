a63deeb02e17b3384fb01f9503f55288070021d5
Crapteep <crapteep@gmail.com>
2023-07-03 14:39:03 +0200

    Bug with duplicate files fixed

--- frontend/src/components/DisplaySingleUserImage/DisplaySingleUserImage.js
+++ frontend/src/components/DisplaySingleUserImage/DisplaySingleUserImage.js
@@ -1,12 +1,16 @@
 import "./DisplaySingleUserImage.css"
 
+
+const URL = process.env.REACT_APP_API_URL
+
 const DisplaySingleUserImage = ( {image} ) => {
     return (
         <div className="col-12 col-sm-10 col-md-10 col-lg-4 mb-2 mt-2 d-flex justify-content-center align-items-center">
             <img 
-                src={`http://localhost:8000/static/${image}`}
-                alt={`Box Image`}
+                src={`${URL}/static/${image}`}
+                alt={image.slice(0,5)}
                 className="img-fluid fixed-size-image border border-gray-400"/>
+            
         </div>
     )
 }

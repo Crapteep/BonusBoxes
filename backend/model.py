dc488444aee79d7dfd92efc1f0aaafa3d5213ad1
Crapteep <crapteep@gmail.com>
2023-07-11 15:52:29 +0200

    new models added and existing ones updated

--- backend/model.py
+++ backend/model.py
@@ -1,12 +1,13 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, EmailStr
 from datetime import datetime
 
 
 class User(BaseModel):
-    name: str
+    user_id: str
     box: str | None = None
     mbox: str | None = None
     gbox: str | None = None
+    expired: bool = False
 
 
 class Post(BaseModel):
@@ -14,7 +15,23 @@ class Post(BaseModel):
     description: str | None = None
     users: list[User]
     created_at: datetime | None = None
+    expired: bool = False
 
     def __init__(self, **data):
         super().__init__(**data)
         self.created_at = datetime.now()
+
+
+class Account(BaseModel):
+    email: EmailStr
+    username: str | None = None
+    password: str
+    last_update: datetime | None = None
+
+
+class DeleteAccount(BaseModel):
+    email: EmailStr
+
+
+class DeleteAccounts(BaseModel):
+    emails: list[EmailStr]

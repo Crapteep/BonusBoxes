605e0acf2204a842a8ab557b3a77127e1b66fa41
Crapteep <crapteep@gmail.com>
2023-07-11 15:49:34 +0200

    new functions have been made for retrieving/deleting/adding records in the database

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,12 @@
 import motor.motor_asyncio
-from model import Post
-from datetime import datetime, time
+from model import Post, Account
+from datetime import datetime, time, timedelta
 from config import get_settings
 import os
+from bson import ObjectId
+from bson.errors import InvalidId
+from fastapi import HTTPException
+
 
 settings = get_settings()
 
@@ -10,6 +14,8 @@ settings = get_settings()
 client = motor.motor_asyncio.AsyncIOMotorClient(settings.db_url)
 database = client.BonusBoxes
 posts_collection = database.posts
+accounts_collection = database.accounts
+settings_collection = database.settings
 
 
 async def check_posts_today():
@@ -47,4 +53,90 @@ async def update_post_users(post_id, users):
 
 async def find_user_by_name(username):
     result = await posts_collection.find_one({'users.name': username})
+    return result
+
+
+async def check_did_account_exists_by_accountID(account_id):
+    try:
+        document = await accounts_collection.find_one({"_id": ObjectId(account_id)})
+        return document
+
+    except InvalidId:
+        raise HTTPException(400, detail="Invalid account ID")
+    
+
+async def check_did_account_exists_by_email(email):
+    document = await accounts_collection.find_one({"email": email})
+    return document
+
+
+async def insert_new_account(account):
+    document = account
+    result = await accounts_collection.insert_one(document)
+    return result
+
+
+async def update_account(account_id):
+    result = await accounts_collection.update_one({"_id": ObjectId(account_id)}, {"$set": {"last_update": datetime.now()}})
+    print(result)
+    return result
+
+
+async def fetch_username_from_all_accounts():
+    accounts = []
+    cursor = accounts_collection.find({}, {'username': 1})
+    async for account in cursor:
+        accounts.append(account["username"])
+    return accounts
+
+
+async def fetch_hashed_password():
+    hashed_password = None
+    document = await settings_collection.find_one({})
+    if document and 'hashed_password' in document:
+        hashed_password = document['hashed_password']
+    return hashed_password
+
+
+async def fetch_accounts_with_data():
+    accounts = []
+    cursor = accounts_collection.find({})
+    async for account in cursor:
+        accounts.append(Account(**account))
+    return accounts
+
+
+async def fetch_documents_older_than_one_day():
+    one_day_ago = datetime.now() - timedelta(days=1)
+
+    pipeline = [
+        {
+            "$match": {
+                "$or": [
+                    {"last_update": None},
+                    {"last_update": {"$lt": one_day_ago}}
+                ],
+            }
+        }
+    ]
+
+    cursor = accounts_collection.aggregate(pipeline)
+    results = await cursor.to_list(length=None)
+    for result in results:
+        result["_id"] = str(result["_id"])
+
+    sorted_results = sorted(results, key=lambda x: x.get("last_update") or datetime.min, reverse=False)
+    return sorted_results
+
+
+async def delete_account_by_email(email):
+    account_exists = await check_did_account_exists_by_email(email)
+    if account_exists:
+        await accounts_collection.delete_one({"email": email})
+        return {"email": email}
+    raise HTTPException(404, f"There is no account with this email {email}")
+    
+
+async def delete_many_accounts(accounts):
+    result = await accounts_collection.delete_many({"email": {"$in": accounts.emails}})
     return result

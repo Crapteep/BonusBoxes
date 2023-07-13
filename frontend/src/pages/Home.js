709e6a6cdaa9d900fb8835c59374dded23ba082f
Crapteep <crapteep@gmail.com>
2023-07-13 13:42:58 +0200

    fix error

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -7,7 +7,7 @@ import usePostsData from "../hooks/usePostsData";
 
 const Home = () => {
   const { isLoading, data } = usePostsData();
-  document.title = "BonusBoxes - Home"
+  document.title = "BonusBoxes - Home";
   if (isLoading) {
     return (
       <div className="container">
@@ -16,48 +16,52 @@ const Home = () => {
     );
   }
 
-  if (!data || data.length === 0) {
-    return (
-    <div className="container my-5 d-flex justify-content-center align-items-center">
-    <p>There are no available posts.</p>
-    </div>
-    );
-    }
-
   return (
     <div className="container">
-      {data?.map((post, index) => (
-        <Link to={`/posts/${post.title}`} style={{ textDecoration: "none" }} key={index}>
-          <div className="row card my-3 border-1">
-            <div className="card-header d-flex justify-content-between align-items-center">
-              <div className="d-flex align-items-center">
-                <h5>{post.title}</h5>
-                {post.expired && (
-                  <p
-                    className="text-red-700 text-sm my-2"
-                    style={{
-                      textAlign: "right",
-                      color: "red",
-                      fontSize: "0.7rem",
-                      marginLeft: "10px",
-                    }}
-                  >
-                    Expired!
-                  </p>
-                )}
+      {data?.length > 0 ? (
+        data.map((post, index) => (
+          <Link
+            to={`/posts/${post.title}`}
+            style={{ textDecoration: "none" }}
+            key={index}
+          >
+            <div className="row card my-3 border-1">
+              <div className="card-header d-flex justify-content-between align-items-center">
+                <div className="d-flex align-items-center">
+                  <h5>{post.title}</h5>
+                  {post.expired && (
+                    <p
+                      className="text-red-700 text-sm my-2"
+                      style={{
+                        textAlign: "right",
+                        color: "red",
+                        fontSize: "0.7rem",
+                        marginLeft: "10px",
+                      }}
+                    >
+                      Expired!
+                    </p>
+                  )}
+                </div>
+                <div className="d-flex align-items-center">
+                  {post.users.length}
+                  <FiUsers className="m1-auto" />
+                </div>
               </div>
-              <div className="d-flex align-items-center">
-                {post.users.length}
-                <FiUsers className="m1-auto" />
-              </div>
-            </div>
 
-            <div className="card-body">
-              {post.description ? post.description : "There is no description."}
+              <div className="card-body">
+                {post.description
+                  ? post.description
+                  : "There is no description."}
+              </div>
             </div>
-          </div>
-        </Link>
-      ))}
+          </Link>
+        ))
+      ) : (
+        <div className="container my-5 d-flex justify-content-center align-items-center">
+          <p>There are no available posts.</p>
+        </div>
+      )}
       <div style={{ marginBottom: "80px" }}></div>
     </div>
   );

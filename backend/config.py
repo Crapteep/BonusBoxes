d6930653abbbd795fbf2b60a1ba3feabbe576975
Crapteep <crapteep@gmail.com>
2023-07-13 14:02:25 +0200

    adding a key to the settings

--- backend/config.py
+++ backend/config.py
@@ -7,20 +7,23 @@ class Settings(BaseSettings):
     env_name: str = "Development"
     base_url: str = "http://localhost:8000"
     db_url: str = "mongodb://localhost:27017"
+    key: str = ""
 
     class Config:
         env_file = ".env"
-    
+
 
 @lru_cache
 def get_settings() -> Settings:
 
     env_name = os.getenv("ENV_NAME", "").lower()
     db_url = os.getenv("DB_URL", "")
+    key = os.getenv("KEY", "")
 
     if env_name == "production":
         Settings.Config.env_file = ".env.production"
         Settings.db_url = db_url
+        Settings.key = key
 
     settings = Settings()
     print(f"Loading settings for: {settings.env_name}")
